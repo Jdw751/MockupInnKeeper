8774fbf1a6a8d373c6d94cca61c739ed4cf24758
Leress <jdw751@msstate.edu>
2016-09-06 18:20:45 -0400

    minor edits
    ...

--- mockuppanel.js
+++ mockuppanel.js
@@ -1,9 +1,50 @@
 
-
+// slide out panel functions
 function openNav() {
     document.getElementById("mySidenav").style.width = "250px";
 }
 
 function closeNav() {
     document.getElementById("mySidenav").style.width = "0";
+}
+
+// add to list functions
+
+var list = [];
+
+document.getElementById("list").innerHTML = list;
+
+display = document.getElementById("list");
+
+var displayedList = ""
+
+
+function addToList(itemID) {
+
+	var item = itemID.toString();
+ 	list.push(item);
+ 	
+ }
+
+function removeFromList(indexToRemove){
+	list.splice(indexToRemove,1);
+
+}
+
+function displayMyList(){
+
+
+	var displayedList = "";
+	var listCount = list.length;
+
+
+	for(var i = 0; i < listCount; i++){
+
+		displayedList += "<li>"+ list[i] + "  " +"<input type='checkbox'  onclick='removeFromList("+i+");displayMyList()'>" + "</li>";
+
+
+	}
+	 document.getElementById("list").innerHTML = displayedList;
+
+
 }
